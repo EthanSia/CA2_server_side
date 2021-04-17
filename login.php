40140a785f7e497def3653329f4507032dad27fb
Ethan <D00225319@student.dkit.ie>
2021-04-17 19:52:25 +0100

    Change the input type to password in login.php

--- login.php
+++ login.php
@@ -85,7 +85,7 @@ include('includes/header.php');
             <label for="username">Username</label>
             <input type="text" id="username" name="username" required><br>
             <label for="password">Password</label>
-            <input type="text" id="password" name="password" required><br>
+            <input type="password" id="password" name="password" required><br>
             <input type="submit" name="login" value="Login">
             <p class="errors">
   		       <?php echo $errors?>
